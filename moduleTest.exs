a127c36663b15b0391c7be04b8da50a6ea9b0913
nooysters <self@jeremyazzari.com>
2017-04-02 17:10:32 -0700

    Floyd lives

--- moduleTest.exs
+++ moduleTest.exs
@@ -19,14 +19,39 @@ defmodule Times do
 end
 
 
-
 defmodule Factorial do
+
   def of(0), do: 1 
   def of(n), do: n * of(n-1) 
 
   def gcd(x,0), do: x
   def gcd(x,y), do: gcd(y, rem(x,y))
+
 end
 
 
+defmodule Floyd do
 
+  def guess_my_number(_, number) when is_integer(number), do: number
+
+  def guess_my_number(actual, range) when is_map(range) do 
+    first.._ = range
+    count = Enum.count(range)
+    my_guess = first + div(count, 2)
+    IO.puts "guess #{my_guess}"
+    guess_my_number(actual, guess(my_guess, actual, range))
+  end
+
+  def guess(my_guess, actual, _) when my_guess === actual, do: my_guess
+
+  def guess(my_guess, actual, range) when my_guess > actual do
+    first.._ = range
+    first..(my_guess-1)
+  end
+
+  def guess(my_guess, actual, range) when my_guess < actual do
+    _..last = range
+    (my_guess+1)..last
+  end
+
+end
